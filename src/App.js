d147c3c2a178ee4f33e8c0a3d596753f23fd050f
azizbenzina <73591945+azizbenzina@users.noreply.github.com>
2022-04-14 07:32:00 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MovieCard from './MovieCard';
+import MovieList from './MovieList';
+
+function App() {
+  return (
+    <div className="App">
+     <MovieList/>
+    </div>
+  );
+}
+
+export default App;
